8651c2bcc5d28824f7361371976375c1085db4bc
Phurten Jang Sherpa <n01716188@humber.ca>
2025-09-10 16:42:48 -0400

    week 2

---
+++ week2/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>PHP Page</title>
+</head> 
+<body>
+    <?php
+    $fname = "Phurten";
+    echo "Welcome back " . $fname;
+    echo "<h1>" . $fname . "</h1>";
+    ?>
+</body>
+</html>
